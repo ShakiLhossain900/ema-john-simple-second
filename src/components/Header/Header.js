90475b39e0afe7646f1be6b7f0739ea2fd604de6
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-02-19 00:13:34 +0600

    adding someting new

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <nav>
                 <a href="/shop"> Shop</a>
                 <a href="/review">Order review</a>
-                <a href="/manage">Manage Inventory</a>
+                <a href="/manage">Order</a>
                 </nav>
         </div>
     );
